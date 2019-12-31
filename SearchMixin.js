40d607aa874a8864385eeb3f76eafa3083a56189
Travis Reynolds <travis@cirruscreative.co.uk>
2019-12-31 22:12:32 +0000

    Add fallback in case search hasn't loaded

--- SearchMixin.js
+++ SearchMixin.js
@@ -4,6 +4,7 @@ export default {
   }),
   computed: {
     searchResults () {
+      if (!this.$search) return []
       const searchTerm = this.searchTerm
       if (searchTerm.length < 3) return []
       return this.$search.search({ query: searchTerm, limit: 8 })
