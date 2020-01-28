5b48d153d44afda970a53fca3eea69b5fb510451
Travis Reynolds <travis@cirruscreative.co.uk>
2020-01-28 17:42:45 +0000

    Use !== development instead of if production

--- gridsome.client.js
+++ gridsome.client.js
@@ -2,7 +2,7 @@ import FlexSearch from 'flexsearch'
 
 export default async function (Vue, { flexsearch, searchFields, pathPrefix, siteUrl }, { isClient }) {
   if (isClient) {
-    const indexPath = pathPrefix && (process.env.NODE_ENV === 'production' || location.origin === siteUrl) ? `${pathPrefix}/flexsearch.json` : '/flexsearch.json'
+    const indexPath = pathPrefix && (process.env.NODE_ENV !== 'development' || location.origin === siteUrl) ? `${pathPrefix}/flexsearch.json` : '/flexsearch.json'
     const searchIndex = await fetch(indexPath).then(r => r.json())
     const search = new FlexSearch({
       ...flexsearch,
