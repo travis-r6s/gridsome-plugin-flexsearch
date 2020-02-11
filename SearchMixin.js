421ad680e147a71894327308e60f48947b3a0f02
Travis Reynolds <travis@cirruscreative.co.uk>
2020-02-11 23:36:50 +0000

    Remove !$search check

--- SearchMixin.js
+++ SearchMixin.js
@@ -4,7 +4,6 @@ export default {
   }),
   computed: {
     searchResults () {
-      if (!this.$search) return []
       const searchTerm = this.searchTerm
       if (searchTerm.length < 3) return []
       return this.$search.search({ query: searchTerm, limit: 8 })
