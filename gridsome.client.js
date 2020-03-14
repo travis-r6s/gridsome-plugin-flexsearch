be7b5d4f858b68dbac72ccad0957e0af6202dd16
Travis Reynolds <travis@cirruscreative.co.uk>
2020-03-14 20:54:01 +0000

    Add chunked mode

--- gridsome.client.js
+++ gridsome.client.js
@@ -1,9 +1,10 @@
 import FlexSearch from 'flexsearch'
 
-export default async function (Vue, { flexsearch, searchFields, pathPrefix, siteUrl }, { isClient }) {
+export default async function (Vue, { flexsearch, chunk = false, searchFields, pathPrefix, siteUrl }, { isClient }) {
   if (isClient) {
-    const indexPath = pathPrefix && (process.env.NODE_ENV !== 'development' || location.origin === siteUrl) ? `${pathPrefix}/flexsearch.json` : '/flexsearch.json'
-    const searchIndex = await fetch(indexPath).then(r => r.json())
+    const basePath = pathPrefix && (process.env.NODE_ENV !== 'development' || location.origin === siteUrl) ? `${pathPrefix}/flexsearch` : '/flexsearch'
+
+    // Setup global Flexsearch Instance
     const search = new FlexSearch({
       ...flexsearch,
       doc: {
@@ -11,7 +12,23 @@ export default async function (Vue, { flexsearch, searchFields, pathPrefix, site
         field: searchFields
       }
     })
-    search.import(searchIndex, { serialize: false })
     Vue.prototype.$search = search
+
+    if (!chunk) {
+      const searchIndex = await fetch(`${basePath}.json`).then(r => r.json())
+      search.import(searchIndex, { serialize: false })
+    } else {
+      const { index, docs } = await fetch(`${basePath}/manifest.json`).then(r => r.json())
+
+      const fetchData = id => fetch(`${basePath}/${id}.json`).then(r => r.json())
+
+      const indexPromises = index.map(id => fetchData(id))
+      const docsPromises = docs.map(id => fetchData(id))
+
+      const searchIndex = await Promise.all(indexPromises)
+      const searchDocs = (await Promise.all(docsPromises)).flat().reduce((docs, [id, doc]) => ({ ...docs, [ id ]: doc }), {})
+
+      search.import(searchIndex, { index: true, doc: searchDocs, serialize: false })
+    }
   }
 }
--- gridsome.server.js
+++ gridsome.server.js
@@ -1,9 +1,11 @@
 const path = require('path')
 const fs = require('fs')
 const FlexSearch = require('flexsearch')
+const { v4: uuid } = require('uuid')
+const _chunk = require('lodash.chunk')
 
 function CreateSearchIndex (api, options) {
-  const { searchFields = [], collections = [], flexsearch = {} } = options
+  const { searchFields = [], collections = [], flexsearch = {}, chunk = false } = options
   const { profile = 'default', ...flexoptions } = flexsearch
 
   const collectionsToInclude = collections.map(({ typeName }) => typeName)
@@ -43,20 +45,86 @@ function CreateSearchIndex (api, options) {
 
   api.configureServer(app => {
     console.log('Serving search index...')
-    const searchIndex = search.export({ serialize: false })
-    app.get('/flexsearch.json', (req, res) => {
-      res.json(searchIndex)
-    })
+    if (chunk) {
+      const { manifest, chunks } = createManifest()
+      app.get('/flexsearch/manifest.json', (req, res) => {
+        res.json(manifest)
+      })
+      app.get('/flexsearch/:chunk', (req, res) => {
+        const chunkName = req.params.chunk.replace('.json', '')
+        if (!chunk) res.status(404).send(`That chunk can't be found.`)
+        res.json(chunks[ chunkName ])
+      })
+    } else {
+      const searchIndex = search.export({ serialize: false })
+      app.get('/flexsearch.json', (req, res) => {
+        res.json(searchIndex)
+      })
+    }
   })
 
   api.afterBuild(async ({ config }) => {
-    console.log('Saving search index...')
     const outputDir = config.outputDir || config.outDir
-    const filename = path.join(outputDir, 'flexsearch.json')
-    const searchIndex = search.export({ serialize: false })
-    await fs.writeFileSync(filename, JSON.stringify(searchIndex))
-    console.log('Saved search index.')
+
+    if (chunk) {
+      console.log('Creating search index (chunked mode)...')
+      const flexsearchDir = path.join(outputDir, 'flexsearch')
+      const manifestFilename = path.join(flexsearchDir, 'manifest.json')
+
+      const { manifest, chunks } = createManifest()
+
+      await fs.mkdirSync(flexsearchDir)
+      await fs.writeFileSync(manifestFilename, JSON.stringify(manifest))
+
+      for (const [name, data] of Object.entries(chunks)) {
+        const chunkFilename = path.join(flexsearchDir, `${name}.json`)
+        await fs.writeFileSync(chunkFilename, JSON.stringify(data))
+      }
+
+      console.log('Saved search index.')
+    } else {
+      console.log('Creating search index...')
+      const filename = path.join(outputDir, 'flexsearch.json')
+      const searchIndex = search.export({ serialize: false })
+      await fs.writeFileSync(filename, JSON.stringify(searchIndex))
+      console.log('Saved search index.')
+    }
   })
+
+  function createManifest () {
+    const searchIndex = search.export({ serialize: false, index: true, doc: false })
+    const [searchDocs] = search.export({ serialize: false, index: false, doc: true })
+
+    const chunkedIndex = searchIndex.reduce((manifest, index) => {
+      const chunk = { id: uuid(), index }
+      return {
+        ids: [...manifest.ids, chunk.id],
+        indexes: {
+          ...manifest.indexes,
+          [ chunk.id ]: chunk.index
+        }
+      }
+    }, { ids: [], indexes: {} })
+
+    const chunkedDocs = _chunk(Object.entries(searchDocs), 2000).reduce((manifest, docs) => {
+      const chunk = { id: uuid(), docs }
+      return {
+        ids: [...manifest.ids, chunk.id],
+        docs: {
+          ...manifest.docs,
+          [ chunk.id ]: chunk.docs
+        }
+      }
+    }, { ids: [], docs: {} })
+
+    const manifest = {
+      hash: uuid(),
+      index: chunkedIndex.ids,
+      docs: chunkedDocs.ids
+    }
+
+    return { manifest, chunks: { ...chunkedDocs.docs, ...chunkedIndex.indexes } }
+  }
 }
 
 module.exports = CreateSearchIndex
